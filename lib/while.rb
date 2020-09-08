de5aaed8413349b6b7cc3b7f35f0e6d04268ca09
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-08 23:59:24 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,8 @@
 def using_while
-  
-end
-
-levitation_force = 6
+ levitation_force = 6
   while levitation_force < 10 
   levitation_force += 1 
-  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa" 
+end
 end
 
